bda393c1b1c8cecddf9cdd02eaf30f4d119a343a
PatrickNorton <patrick.147.norton@gmail.com>
2018-11-27 18:30:05 -0500

    Added theboard.enemypcs generator

--- Shogiclasses.py
+++ Shogiclasses.py
@@ -282,6 +282,9 @@ class board:
         if not isinstance(self[movedto], nopiece):
             raise IllegalMove
 
+    def enemypcs(self):
+        yield from [x for x in self if x.COLOR != self.currplyr.other()]
+
 
 class IllegalMove(Exception):
     pass
