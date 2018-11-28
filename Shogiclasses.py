86fbf4845bf554063e542285fe6c46bbb3f82ab3
PatrickNorton <patrick.147.norton@gmail.com>
2018-11-27 19:08:04 -0500

    Changed coord.__init__ to properly handle str inputs

--- Shogiclasses.py
+++ Shogiclasses.py
@@ -187,8 +187,8 @@ class direction:
 class coord:
     def __init__(self, xy):
         if isinstance(xy, str):
-            self.x = '987654321'.index(xy[0])
-            self.y = 'abcdefghi'.index(xy[1])
+            self.x = 'abcdefghi'.index(xy[0])
+            self.y = '987654321'.index(xy[1])
         else:
             self.x = xy[0]
             self.y = xy[1]
