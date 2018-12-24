a08eaca01a0124ca5b44d2d09b1d11a9e481fe7a
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-24 11:10:25 -0500

    Added main loop to __main__

--- __main__.py
+++ __main__.py
@@ -3,4 +3,63 @@ from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit import print_formatted_text as print
 import Shogi
 import Shogiclasses
-aboard = Shogiclasses.board()
+import json
+def main():
+    theboard = Shogiclasses.board()
+    game = True
+    debug = False
+    errstr = ''
+    if debug:
+        theboard = Shogi.setpos()
+    with open('shogierrors.json') as etxt:
+        errorlist = json.load(etxt)
+    while game:
+        if errstr:
+            print('<b>'+errstr+'</b>')
+            errstr = ''
+        print(str(theboard))
+        print(f"{repr(theboard.currplyr)}'s turn")
+        try:
+            pieceloc = Shogi.piececheck(stdscr, theboard)
+            coords = Shogi.movecheck(stdscr, theboard, pieceloc)
+            Shogi.movecheck2(theboard, coords)
+            tocc = (theboard, theboard.nextmove, theboard.currplyr, True)
+            ccvars = Shogi.checkcheck(*tocc)
+            check = ccvars[0]
+            if check:
+                raise Shogiclasses.IllegalMove(6)
+        except Shogiclasses.IllegalMove as e:
+            var = int(str(e))
+            if var:
+                errstr = f"Error: {errorlist[var]}"
+            continue
+        except Shogi.OtherMove:
+            theboard.currplyr = theboard.currplyr.other()
+            continue
+        theboard.move(*theboard.nextmove)
+        moveloc = theboard.nextmove[1]
+        promote = theboard.canpromote(moveloc)
+        canpromote = theboard[moveloc].PROMOTABLE
+        ispromoted = theboard[moveloc].prom
+        if promote and canpromote and not ispromoted:
+            if theboard.autopromote(moveloc):
+                theboard.promote(moveloc)
+            else:
+                topromote = getinput(stdscr, 'Promote this piece?', True)
+                if topromote:
+                    theboard.promote(moveloc)
+        theboard.lastmove = theboard.nextmove
+        clr = theboard.currplyr.other()
+        ccvars = Shogi.checkcheck(theboard, theboard.lastmove, clr)
+        check, kingpos, checklist = ccvars
+        if check and game:
+            mate = Shogi.matecheck(theboard, kingpos, checklist)
+            game = not mate
+            if mate:
+                print(theboard)
+                print(f"Checkmate. {repr(theboard.currplyr)} wins")
+                game = False
+                break
+            else:
+                print('Check')
+        theboard.currplyr = theboard.currplyr.other()
