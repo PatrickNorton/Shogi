f8c2ac08300921463eaabdc841731ebd04fde255
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-25 06:24:50 -0500

    Added filedisp function

--- Shogitxt.py
+++ Shogitxt.py
@@ -65,8 +65,9 @@ def helpdesk(input_gen, window, theboard, filenm=None):
             print(toout)
         return
     prompt = 'Press Esc to activate help menu'
-    filedisp(input_gen, window, prompt, filetxt)
+    todisp = filedisp(input_gen, window, prompt, filetxt)
     while True:
+        todisp = todisp[:-1]
         todisp.append('help: ')
         filenm = getinput(input_gen, window, todisp)
         filenm = filenm.strip()
@@ -84,7 +85,7 @@ def helpdesk(input_gen, window, theboard, filenm=None):
                 with open(f"helpfiles/{filenm}.txt") as f:
                     thefile = f.read()
                 prompt = 'Press Esc to activate help menu'
-                filedisp(input_gen, window, prompt, filetxt)
+                todisp = filedisp(input_gen, window, prompt, filetxt)
             except FileNotFoundError:
                 print('Invalid help command\n')
                 with open("helpfiles/helpcommands.txt") as f:
@@ -225,4 +226,35 @@ def yninput(input_gen, window, todisp):
 
 
 def filedisp(input_gen, window, prompt, filetxt):
-    pass
+    maxx = window.width
+    maxy = window.height
+    filetxt = filetxt.split('\n')
+    filelist = []
+    for lin in filetxt:
+        if len(lin) >= maxx-1:
+            filelist.append(lin[:maxx-1])
+            filelist.append(lin[:maxx-1])
+        else:
+            filelist.append(lin)
+    scrollable = True
+    while len(filelist) < maxy-2:
+        scrollable = False
+        filelist.append('')
+    firstline = 0
+    todisp = filetxt[:maxy-2]
+    todisp.append(prompt)
+    window.render_to_terminal(todisp)
+    for c in input_gen:
+        if scrollable:
+            if c == '<UP>':
+                if firstline > 0:
+                    firstline -= 1
+            if c == '<DOWN>':
+                if firstline+maxy-2 < len(filelist):
+                    firstline += 1
+        if c == '<ESC>':
+            break
+        todisp = filetxt[firstline:firstline+maxy-2]
+        todisp.append(prompt)
+        window.render_to_terminal(todisp)
+    return todisp
