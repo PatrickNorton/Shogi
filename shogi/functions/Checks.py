da9d6b3c3c7aeecc4add2be2101e1be8f19ed5fc
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-25 17:27:24 -0500

    Removed from imports in checks.py

--- shogi/functions/Checks.py
+++ shogi/functions/Checks.py
@@ -1,5 +1,4 @@
-from shogi.classes import piece, board, direction, coord
-from shogi.classes import IllegalMove, row, color
+from shogi import classes
 from itertools import product
 
 
@@ -18,18 +17,18 @@ class OtherInput(Exception):
 def piececheck(theboard, pieceloc):
     validpiece = inputpiece(theboard, pieceloc)
     if not validpiece:
-        raise IllegalMove(11)
-    pieceloc = coord(pieceloc)
+        raise classes.IllegalMove(11)
+    pieceloc = classes.coord(pieceloc)
     if theboard[pieceloc].COLOR != theboard.currplyr:
-        raise IllegalMove(5)
+        raise classes.IllegalMove(5)
     return pieceloc
 
 
 def movecheck(theboard, current, moveloc):
     validpiece = inputpiece(theboard, moveloc)
     if not validpiece:
-        raise IllegalMove(11)
-    moveloc = coord(moveloc)
+        raise classes.IllegalMove(11)
+    moveloc = classes.coord(moveloc)
     return (current, moveloc)
 
 
@@ -37,14 +36,14 @@ def movecheck2(theboard, coords):
     current, new = coords
     piece = theboard[current]
     move = new-current
-    movedir = direction(move)
+    movedir = classes.direction(move)
     magicvar = piece.MOVES[movedir]
-    if movedir == direction(8):
-        raise IllegalMove(3)
+    if movedir == classes.direction(8):
+        raise classes.IllegalMove(3)
     elif not piece.canmove(move):
-        raise IllegalMove(1)
+        raise classes.IllegalMove(1)
     elif theboard[new].COLOR == theboard[current].COLOR:
-        raise IllegalMove(4)
+        raise classes.IllegalMove(4)
     elif magicvar == 'T':
         pass
     elif str(piece.PTYPE) == 'k':
@@ -54,22 +53,22 @@ def movecheck2(theboard, coords):
 
 
 def obscheck(theboard, current, move):
-    movedir = direction(move)
+    movedir = classes.direction(move)
     for x in range(1, max(abs(move))):
         relcoord = [x*k for k in movedir]
-        testpos = current+coord(relcoord)
+        testpos = current+classes.coord(relcoord)
         if theboard[testpos]:
-            raise IllegalMove(2)
+            raise classes.IllegalMove(2)
 
 
 def checkcheck(theboard, coords, color, earlybreak=False):
     oldloc, newloc = coords
     check, checklist = False, []
-    toget = piece('k', color)
+    toget = classes.piece('k', color)
     kingpos = theboard.getpiece(toget)
     try:
         movecheck2(theboard, (newloc, kingpos))
-    except IllegalMove:
+    except classes.IllegalMove:
         tocc2 = ((oldloc, kingpos), [], earlybreak)
         check, checklist = checkcheck2(theboard, *tocc2)
     else:
@@ -90,14 +89,14 @@ def checkcheck2(theboard, coords, checklist, earlybreak=False):
     mvmt = abs(relcoord)
     if mvmt.x != mvmt.y and min(mvmt):
         return False, checklist
-    toking = direction(relcoord)
-    doa = row(oldloc, toking)
+    toking = classes.direction(relcoord)
+    doa = classes.row(oldloc, toking)
     currpieces = theboard.playerpcs(theboard[kingpos].COLOR.other())
     pieces = (x for x in doa if x in currpieces)
     for x in pieces:
         try:
             movecheck2(theboard, (x, kingpos))
-        except IllegalMove:
+        except classes.IllegalMove:
             continue
         else:
             checklist.append(x)
@@ -110,44 +109,44 @@ def checkcheck2(theboard, coords, checklist, earlybreak=False):
 
 def kingcheck(theboard, coords):
     oldloc, newloc = coords
-    rowlist = (direction(x) for x in range(8))
+    rowlist = (classes.direction(x) for x in range(8))
     for x, dist in product(rowlist, range(9)):
-        dist = coord(dist)
+        dist = classes.coord(dist)
         try:
             loctotest = newloc+x*dist
             if theboard[loctotest].COLOR == theboard[oldloc].COLOR:
-                raise IllegalMove(4)
+                raise classes.IllegalMove(4)
             movecheck2(theboard, (loctotest, newloc))
         except (ValueError, IndexError):
             continue
-        except IllegalMove as e:
+        except classes.IllegalMove as e:
             if str(e) == '2':
                 break
             else:
                 continue
         else:
-            raise IllegalMove(6)
+            raise classes.IllegalMove(6)
     for delx, dely in product((-1, 1), (-1, 1)):
-        relcoord = coord((delx, 2*dely))
+        relcoord = classes.coord((delx, 2*dely))
         try:
             abscoord = newloc+relcoord
             movecheck2(theboard, (abscoord, newloc))
         except (ValueError, IndexError):
             continue
-        except IllegalMove:
+        except classes.IllegalMove:
             continue
         else:
-            raise IllegalMove(6)
+            raise classes.IllegalMove(6)
 
 
 def matecheck(theboard, kingpos, checklist):
-    kingmovepos = (coord(direction(x)) for x in range(8))
+    kingmovepos = (classes.direction(x) for x in range(8))
     for kmpiter in kingmovepos:
         newpos = kmpiter+kingpos
         if tuple(newpos) in theboard.it():
             try:
                 movecheck2(theboard, (kingpos, newpos))
-            except IllegalMove:
+            except classes.IllegalMove:
                 continue
             else:
                 return False
@@ -162,16 +161,16 @@ def matecheck(theboard, kingpos, checklist):
     for loc in theboard.enemypcs():
         try:
             movecheck2(theboard, (loc, checklist))
-        except IllegalMove:
+        except classes.IllegalMove:
             continue
         return False
     move = kingpos-checklist
-    movedir = direction(move)
+    movedir = classes.direction(move)
     for pos, z in product(theboard.enemypcs(), range(abs(max(move)))):
-        newpos = z*checklist*coord(movedir)
+        newpos = z*checklist*classes.coord(movedir)
         try:
             movecheck2(theboard, (pos, newpos))
-        except IllegalMove:
+        except classes.IllegalMove:
             continue
         return False
     return True
@@ -179,7 +178,7 @@ def matecheck(theboard, kingpos, checklist):
 
 def inputpiece(theboard, pieceloc):
     try:
-        pieceloc = coord(pieceloc)
+        pieceloc = classes.coord(pieceloc)
         return True
     except (ValueError, IndexError):
         raise OtherInput(pieceloc)
