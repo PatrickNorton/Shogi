316332824a621c3ebfe48926caf9abbb37e50488
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-26 09:46:37 -0500

    Added kingcheck to movecheck file

--- shogi/functions/boardtests/movecheck.py
+++ shogi/functions/boardtests/movecheck.py
@@ -1,5 +1,5 @@
+from itertools import product
 from shogi import classes
-from .kingcheck import kingcheck
 from .inputpiece import inputpiece
 
 __all__ = [
@@ -43,3 +43,35 @@ def obscheck(theboard, current, move):
         testpos = current+classes.coord(relcoord)
         if theboard[testpos]:
             raise classes.IllegalMove(2)
+
+
+def kingcheck(theboard, coords):
+    oldloc, newloc = coords
+    rowlist = (classes.direction(x) for x in range(8))
+    for x, dist in product(rowlist, range(9)):
+        dist = classes.coord(dist)
+        try:
+            loctotest = newloc+x*dist
+            if theboard[loctotest].COLOR == theboard[oldloc].COLOR:
+                raise classes.IllegalMove(4)
+            movecheck2(theboard, (loctotest, newloc))
+        except (ValueError, IndexError):
+            continue
+        except classes.IllegalMove as e:
+            if str(e) == '2':
+                break
+            else:
+                continue
+        else:
+            raise classes.IllegalMove(6)
+    for delx, dely in product((-1, 1), (-1, 1)):
+        relcoord = classes.coord((delx, 2*dely))
+        try:
+            abscoord = newloc+relcoord
+            movecheck2(theboard, (abscoord, newloc))
+        except (ValueError, IndexError):
+            continue
+        except classes.IllegalMove:
+            continue
+        else:
+            raise classes.IllegalMove(6)
