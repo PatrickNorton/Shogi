d5306864500ef8fafa9a2e99263ea06d2053fe2b
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-28 09:53:18 -0500

    Added docstrings to piece.py

--- shogi/functions/boardtests/piece.py
+++ shogi/functions/boardtests/piece.py
@@ -5,7 +5,22 @@ __all__ = [
     "piececheck"
 ]
 
+
 def piececheck(theboard, pieceloc):
+    """Check if inputted piece is valid.
+
+    Arguments:
+        theboard {board} -- current board state
+        pieceloc {str} -- inputted string of location
+
+    Raises:
+        classes.IllegalMove -- invalid entry of piece
+        classes.IllegalMove -- entry of location without their piece
+
+    Returns:
+        coord -- location inputted
+    """
+
     validpiece = inputpiece(theboard, pieceloc)
     if not validpiece:
         raise classes.IllegalMove(11)
