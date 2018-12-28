dad3aa3c86182bd7f2b57147cdbdd5bd653f1773
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-28 09:24:49 -0500

    Added docstrings for goodinput.py

--- shogi/functions/boardtests/goodinput.py
+++ shogi/functions/boardtests/goodinput.py
@@ -4,7 +4,21 @@ __all__ = [
     "inputpiece"
 ]
 
+
 def inputpiece(theboard, pieceloc):
+    """Test if input is a valid location/
+
+    Arguments:
+        theboard {board} -- current board state
+        pieceloc {str} -- inputted string
+
+    Raises:
+        classes.OtherInput -- if input is not a valid location
+
+    Returns:
+        bool -- is input valid
+    """
+
     try:
         pieceloc = classes.coord(pieceloc)
         return True
