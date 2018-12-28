41e5485d7fd5ab33e73c1e6a1d15742162b1a574
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-28 11:13:24 -0500

    Added docstrings to findmoves.py

--- shogi/functions/inputs/findmoves.py
+++ shogi/functions/inputs/findmoves.py
@@ -5,7 +5,19 @@ __all__ = [
     "testspcs"
 ]
 
+
 def testspcs(theboard, pieceloc, spacelist):
+    """Test which spaces in a list are valid moves.
+
+    Arguments:
+        theboard {board} -- current state of the board
+        pieceloc {coord} -- location of piece to be moved
+        spacelist {list[coord]} -- list of coords to check
+
+    Returns:
+        list -- list of valid spaces
+    """
+
     toreturn = []
     for relloc in spacelist:
         try:
