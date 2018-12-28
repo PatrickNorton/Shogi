f84094c2ba1b7c204e3fd2ff2aadf78ea93f34cd
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-28 07:27:46 -0500

    Added exceptions.py docstrings

--- shogi/classes/exceptions.py
+++ shogi/classes/exceptions.py
@@ -9,28 +9,35 @@ __all__ = [
 ]
 
 class NotPromotableException(Exception):
+    """Piece is not promotable."""
     pass
 
 
 class PromotedException(Exception):
+    """Piece is already promoted."""
     pass
 
 
 class DemotedException(Exception):
+    """Piece is already demoted."""
     pass
 
 
 class IllegalMove(Exception):
+    """The move cannot be made."""
     pass
 
 
 class PlayerExit(Exception):
+    """The player wishes to exit."""
     pass
 
 
 class OtherMove(Exception):
+    """An other move was made."""
     pass
 
 
 class OtherInput(Exception):
+    """A non-location input was entered."""
     pass
