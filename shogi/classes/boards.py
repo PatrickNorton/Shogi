d661150d0489cdc6cc3148aa28c069868168b7e9
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-28 07:25:12 -0500

    Added docstrings for boards.py

--- shogi/classes/boards.py
+++ shogi/classes/boards.py
@@ -9,8 +9,27 @@ __all__ = [
     "board"
 ]
 
+
 class board:
+    """Class for main board object.
+
+    Attributes:
+        PIECES {dict} -- Each coord and its corresponding piece
+        INVPIECES {dict} -- Inverse of PIECES
+        CAPTURED {dict} -- List of captured pieces for each color
+        PCSBYCLR {dict} -- Same as pieces, but seperated by color
+        currplyr {color} -- Active player
+        lastmove {tuple[coord]} -- Previous move performed
+        nextmove {tuple[coord]} -- Move about to be performed
+    """
+
     def __init__(self, pieces=None):
+        """Initialise board.
+
+        Keyword Arguments:
+            pieces {dict} -- for custom board setups (default: {None})
+        """
+
         if pieces is None:
             self.PIECES = {coord(x): piece(*y) for x, y in info.LS.items()}
         else:
@@ -46,11 +65,24 @@ class board:
         toreturn = self.PIECES.get(coords, nopiece())
         return toreturn
 
-    def it(self): yield from ((x, y) for x in range(9) for y in range(9))
+    def it(self):
+        """Yield from all possible board positions."""
 
-    def occupied(self): yield from self.PIECES
+        yield from ((x, y) for x in range(9) for y in range(9))
+
+    def occupied(self):
+        """Yield from currently occupied spaces."""
+
+        yield from self.PIECES
 
     def move(self, current, new):
+        """Move a piece between locations.
+
+        Arguments:
+            current {coord} -- location of piece
+            new {coord} -- location to move piece to
+        """
+
         if not isinstance(self[new], nopiece):
             self.capture(new)
         self.PIECES[coord(new)] = self.PIECES.pop(current)
@@ -59,9 +91,24 @@ class board:
         self.INVPIECES[self[new]] = new
 
     def getpiece(self, location):
+        """Return a location based on piece type.
+
+        Arguments:
+            location {piece} -- piece type to check
+
+        Returns:
+            coord -- location of piece
+        """
+
         return self.INVPIECES[location]
 
     def capture(self, new):
+        """Capture a piece at a location.
+
+        Arguments:
+            new {coord} -- location of to-be-captured piece
+        """
+
         piece = self[new]
         try:
             piece.demote()
@@ -78,16 +125,40 @@ class board:
             del self.INVPIECES[piece]
 
     def canpromote(self, space):
+        """Check if a piece is in a promotion zone.
+
+        Arguments:
+            space {coord} -- location to be checked
+
+        Returns:
+            bool -- if piece is promotable
+        """
+
         zonevar = ((0, 1, 2), (8, 7, 6))
         return space.y in zonevar[int(self.currplyr)]
 
     def autopromote(self, space):
+        """Check if piece must be promoted.
+
+        Arguments:
+            space {coord} -- location to be checked
+
+        Returns:
+            bool -- if piece must promote
+        """
+
         zonevar = ((0, 1, 2), (8, 7, 6))
         plyrint = int(self.currplyr)
         index = zonevar[plyrint].index(space.y)
         return index < self[space].AUTOPROMOTE
 
     def promote(self, space):
+        """Promote the piece at a location.
+
+        Arguments:
+            space {coord} -- space to promote piece at
+        """
+
         piece = self[space]
         piece = piece.promote()
         self.PIECES[space] = piece
@@ -95,6 +166,17 @@ class board:
         self.INVPIECES[piece] = space
 
     def putinplay(self, piece, movedto):
+        """Move a piece from capture into play.
+
+        Arguments:
+            piece {piece} -- the piece to put in play
+            movedto {coord} -- where to put the piece
+
+        Raises:
+            IllegalMove -- if capturing on drop
+            IllegalMove -- if violating two-pawn rule
+        """
+
         player = self.currplyr
         if not isinstance(self[movedto], nopiece):
             raise IllegalMove(8)
@@ -108,8 +190,32 @@ class board:
         self.PIECES[movedto] = piece
         self.INVPIECES[piece] = movedto
 
-    def currpcs(self): return self.PCSBYCLR[self.currplyr]
+    def currpcs(self):
+        """Return pieces of current player.
 
-    def enemypcs(self): return self.PCSBYCLR[self.currplyr.other()]
+        Returns:
+            dict -- current player coords: pieces
+        """
 
-    def playerpcs(self, player): return self.PCSBYCLR[player]
+        return self.PCSBYCLR[self.currplyr]
+
+    def enemypcs(self):
+        """Return pieces of opposing player.
+
+        Returns:
+            dict -- coords to pieces of opp. player
+        """
+
+        return self.PCSBYCLR[self.currplyr.other()]
+
+    def playerpcs(self, player):
+        """Return pieces of specific player
+
+        Arguments:
+            player {color} -- player to return
+
+        Returns:
+            dict -- coords: pieces of player
+        """
+
+        return self.PCSBYCLR[player]
