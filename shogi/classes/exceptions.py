c172bc139384d057b81206456c0e076965cd3235
PatrickNorton <patrick.147.norton@gmail.com>
2018-12-30 20:29:05 -0500

    Added more to exceptions

--- shogi/classes/exceptions.py
+++ shogi/classes/exceptions.py
@@ -12,23 +12,40 @@ __all__ = [
 
 class PromotionError(Exception):
     """Error in promotion"""
+    def __init__(self, errornum=0):
+        super().__init__(errornum)
+        messages = [
+            "Promotion error",
+            "Piece is not promotable",
+            "Piece is already promoted",
+            "Piece is already not promoted"
+        ]
+        self.message = messages[errornum]
+
+    def __str__(self): return self.message
 
 
 class NotPromotableException(PromotionError):
     """Piece is not promotable."""
+    def __init__(self):
+        super().__init__(1)
 
 
 class PromotedException(PromotionError):
     """Piece is already promoted."""
+    def __init__(self):
+        super().__init__(2)
 
 
 class DemotedException(PromotionError):
     """Piece is already demoted."""
+    def __init__(self):
+        super().__init__(3)
 
 
 class IllegalMove(Exception):
     """The move cannot be made."""
-    def __init__(self, errornum):
+    def __init__(self, errornum=0):
         from .information import info
         super().__init__(errornum)
         self.int = errornum
