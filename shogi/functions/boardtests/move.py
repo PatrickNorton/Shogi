3087134948e9304279292310addcba1e7b817759
PatrickNorton <patrick.147.norton@gmail.com>
2019-01-04 11:48:01 -0500

    Added type hints for king_check and obstruction_check

--- shogi/functions/boardtests/move.py
+++ shogi/functions/boardtests/move.py
@@ -31,8 +31,8 @@ def move_check(
 
 
 def move_check_2(
-    current_board: classes.Board,
-    coordinates: Tuple[classes.Coord, classes.Coord]
+        current_board: classes.Board,
+        coordinates: Tuple[classes.Coord, classes.Coord]
 ):
     """Check if piece can be moved between locations.
 
@@ -45,7 +45,7 @@ def move_check_2(
 
     current, new = coordinates
     piece = current_board[current]
-    move = new-current
+    move = new - current
     move_direction = classes.Direction(move)
     move_variable = piece.MOVES[move_direction]
     if move_direction == classes.Direction(8):
@@ -62,7 +62,11 @@ def move_check_2(
         obstruction_check(current_board, current, move)
 
 
-def obstruction_check(current_board, current_position, move_position):
+def obstruction_check(
+        current_board: classes.Board,
+        current_position: classes.Coord,
+        move_position: classes.Coord
+):
     """Check if piece is obstructing move.
 
     :param current_board: current board state
@@ -70,17 +74,19 @@ def obstruction_check(current_board, current_position, move_position):
     :param move_position: location to move piece to
     :raises classes.IllegalMove: obstruction found
     """
-    # TODO: Type hints
 
     move_direction = classes.Direction(move_position)
     for x in range(1, max(abs(move_position))):
-        relative_position = [x*k for k in move_direction]  # TODO: Change to Coord
+        relative_position = [x * k for k in move_direction]  # TODO: Change to Coord
         test_position = current_position + classes.Coord(relative_position)
         if current_board[test_position]:
             raise classes.IllegalMove(2)
 
 
-def king_check(current_board, coordinates):
+def king_check(
+        current_board: classes.Board,
+        coordinates: Tuple[classes.Coord, classes.Coord]
+):
     """Check if king is moving into check.
 
     :param current_board: current board state
@@ -89,14 +95,13 @@ def king_check(current_board, coordinates):
     :raises classes.IllegalMove: attempted move into check
     :raises classes.IllegalMove: attempted move into check
     """
-    # TODO: Type hints
 
     old_location, new_location = coordinates
     direction_set = (classes.Direction(x) for x in range(8))
     for direction, distance in product(direction_set, range(9)):
         distance = classes.Coord(distance)
         try:
-            current_test = new_location+direction*distance
+            current_test = new_location + direction * distance
             if current_board[current_test].samecolor(current_board[old_location]):
                 raise classes.IllegalMove(4)
             move_check_2(current_board, (current_test, new_location))
@@ -110,9 +115,9 @@ def king_check(current_board, coordinates):
         else:
             raise classes.IllegalMove(6)
     for move_x, move_y in product((-1, 1), (-1, 1)):
-        relative_position = classes.Coord((move_x, 2*move_y))
+        relative_position = classes.Coord((move_x, 2 * move_y))
         try:
-            absolute_position = new_location+relative_position
+            absolute_position = new_location + relative_position
             move_check_2(current_board, (absolute_position, new_location))
         except (ValueError, IndexError):
             continue
