848fae4c9ad19dde03d4cb04bb360deea1daf71c
PatrickNorton <patrick.147.norton@gmail.com>
2019-01-11 07:45:10 -0500

    Created first board

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-from kivy.app import App, Widget
+from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.utils import get_color_from_hex
+from shogi import AbsoluteCoord
 
 
 class AppCore(Widget):
-    pass
+    @staticmethod
+    def get_background_color():
+        return get_color_from_hex("#1e2022")
 
 
 class ChessBoard(GridLayout):
     def __init__(self, **kwargs):
-        super().__init__(rows=9, **kwargs)
-        for _ in range(81):
-            self.add_widget(BoardSquare())
-
-
-class Test(Widget):
-    pass
+        super().__init__(cols=9, rows=9, **kwargs)
+        for x in range(81):
+            self.add_widget(BoardSquare(AbsoluteCoord((8-x % 9, x//9))))
 
 
 class BoardSquare(Button):
-    def on_press(self):
-        self.text = 'hi'
+    def __init__(self, position, **kwargs):
+        self.board_position = position
+        super().__init__(**kwargs)
 
-    def on_release(self):
-        self.text = ''
+    def get_position(self): return str(self.board_position)
 
 
 class ShogiBoard(App):
