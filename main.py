ed338c200281a8c0f94c3f8f917f33626fdcf29e
PatrickNorton <patrick.147.norton@gmail.com>
2019-01-13 16:26:14 -0500

    Added docstrings and type checking

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.utils import get_color_from_hex
+from typing import Iterable, Dict, List
 import shogi
 
 # TODO: Promotion, reconstituting captured pieces
@@ -15,37 +16,61 @@ class AppCore(Widget):
 
 
 class ChessBoard(GridLayout):
+    """The class that holds the chess board on screen.
+
+    :ivar children_dict: dict of coordinates -> squares
+    :ivar board: current game board
+    :ivar make_move: if next click makes a move or highlights
+    :ivar move_from: space to move from
+    :ivar in_check: list of pieces checking each king
+    """
     def __init__(self, **kwargs):
+        """Initialise instance of ChessBoard.
+
+        :param kwargs: keyword arguments to pass
+        """
         super().__init__(cols=9, rows=9, **kwargs)
         for x in range(81):
             square = BoardSquare(shogi.AbsoluteCoord((8-x % 9, x//9)))
             self.add_widget(square)
-        self.children_dict = {x.board_position: x for x in self.children}
-        self.board = shogi.Board()
-        self.make_move = False
-        self.move_from = shogi.NullCoord()
-        self.in_check = [[], []]
+        self.children_dict: Dict[shogi.AbsoluteCoord, BoardSquare] = {
+            x.board_position: x for x in self.children
+        }
+        self.board: shogi.Board = shogi.Board()
+        self.make_move: bool = False
+        self.move_from: shogi.AbsoluteCoord = shogi.NullCoord()
+        self.in_check: List[List[shogi.AbsoluteCoord]] = [[], []]
 
-    def space_pressed(self, coordinate):
+    def space_pressed(self, coordinate: shogi.AbsoluteCoord):
+        """Light or make move when a specific space is pressed.
+
+        :param coordinate: location of pressed square
+        """
         if not self.make_move or self.move_from == coordinate:
             self.light_moves(coordinate)
         else:
             self.make_moves(self.move_from, coordinate)
 
-    def light_moves(self, coordinate):
-        pressed_square = self.children_dict[coordinate]
-        pressed_piece = self.board[coordinate]
-        do_highlight = not pressed_square.is_highlighted
-        player_piece = pressed_piece.color == self.board.current_player
+    def light_moves(self, coordinate: shogi.AbsoluteCoord):
+        """Light up legal moves from a coordinate.
+
+        :param coordinate: coordinate to move from
+        """
+        pressed_square: BoardSquare = self.children_dict[coordinate]
+        pressed_piece: shogi.Piece = self.board[coordinate]
+        do_highlight: bool = not pressed_square.is_highlighted
+        players_piece: bool = (
+                pressed_piece.color == self.board.current_player
+        )
         self.un_light_all()
-        if do_highlight and player_piece:
+        if do_highlight and players_piece:
             valid_moves = pressed_square.valid_moves(
                 self.board,
                 self.in_check[pressed_piece.color.int]
             )
-            valid_spaces = [
+            valid_spaces = (
                 self.children_dict[x] for x in valid_moves
-            ]
+            )
             for space in valid_spaces:
                 space.light()
             if self.board[coordinate]:
@@ -55,7 +80,16 @@ class ChessBoard(GridLayout):
         else:
             self.make_move = False
 
-    def make_moves(self, current, to):
+    def make_moves(
+            self,
+            current: shogi.AbsoluteCoord,
+            to: shogi.AbsoluteCoord
+    ):
+        """Move piece between two locations.
+
+        :param current: location of piece
+        :param to: location to move piece to
+        """
         try:
             shogi.move_check_2(self.board, (current, to))
             king_location, checking_own = shogi.check_check(
@@ -77,20 +111,30 @@ class ChessBoard(GridLayout):
                 (current, to),
                 self.board.current_player.other
             )
-            print(is_in_check)
             self.in_check[self.board.current_player.other.int] = is_in_check
             self.board.current_player = self.board.current_player.other
             self.make_move = False
             self.un_light_all()
 
-    def update_squares(self, to_update):
+    def update_squares(self, to_update: Iterable[shogi.AbsoluteCoord]):
+        """Update specific squares.
+
+        :param to_update: list of squares to update
+        """
         for coordinate in to_update:
             space = self.children_dict[coordinate]
             space.text = space.set_string(self.board[coordinate])
 
-    def get_piece(self, position): return self.board[position]
+    def get_piece(self, position: shogi.AbsoluteCoord) -> shogi.Piece:
+        """Get piece at location.
+
+        :param position: position to get piece at
+        :return: piece at location
+        """
+        return self.board[position]
 
     def un_light_all(self):
+        """Un-highlight all squares."""
         highlighted_spaces = {
             x: y for x, y in self.children_dict.items() if y.is_highlighted
         }
@@ -99,32 +143,67 @@ class ChessBoard(GridLayout):
 
 
 class BoardSquare(Button):
-    def __init__(self, position, **kwargs):
-        self.board_position = position
-        self.is_highlighted = False
+    """The class for a board square.
+
+    :ivar board_position: position within board
+    :ivar is_highlighted: whether ot not piece is highlighted
+    """
+    def __init__(self, position: shogi.AbsoluteCoord, **kwargs):
+        """Initialise instance of BoardSquare.
+
+        :param position: location of square on board
+        :param kwargs: standard kwargs for kivy.Button
+        """
+        self.board_position: shogi.AbsoluteCoord = position
+        self.is_highlighted: bool = False
         super().__init__(**kwargs)
 
-    def get_position(self):
+    def get_occupant(self) -> str:
+        """Get string of current occupant of space.
+
+        TODO! Deprecate and/or speed up
+
+        :return: string of occupant
+        """
         pos = shogi.Board()[self.board_position]
         return str(pos) if pos else ''
 
     def light(self):
+        """Highlight self."""
         self.background_normal = "./images/Highlighted space.jpg"
         self.background_down = "./images/Highlighted space.jpg"
         self.color = 0, 0, 0, 1
         self.is_highlighted = True
 
     def un_light(self):
+        """Un-highlight self."""
         self.background_normal = "./images/Light square.jpg"
         self.background_down = "./images/Light square.jpg"
         self.color = 1, 1, 1, 1
         self.is_highlighted = False
 
     @staticmethod
-    def set_string(piece):
+    def set_string(piece: shogi.Piece) -> str:
+        """Get string of a piece.
+
+        Mostly a placeholder for now for images later.
+
+        :param piece: piece to get string of.
+        :return: string of piece
+        """
         return str(piece) if piece else ''
 
-    def valid_moves(self, current_board, checking_spaces):
+    def valid_moves(
+            self,
+            current_board: shogi.Board,
+            checking_spaces: List[shogi.AbsoluteCoord]
+    ) -> List[shogi.AbsoluteCoord]:
+        """Get valid moves for square, given current board.
+
+        :param current_board:
+        :param checking_spaces:
+        :return:
+        """
         current_piece = current_board[self.board_position]
         valid_spaces = []
         for dir_number in range(8):
