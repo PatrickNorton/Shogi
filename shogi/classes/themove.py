0714fa571766b5f6d97e7603e02480020594fe4e
Patrick Norton <patrick.147.norton@gmail.com>
2019-02-26 19:09:20 -0500

    Added Move object input validation

--- shogi/classes/themove.py
+++ shogi/classes/themove.py
@@ -19,8 +19,6 @@ class Move:
             is_drop: bool = False,
             is_capture: bool = False,
             is_promote: Optional[bool] = None,
-            dropped_piece: Optional[Piece] = None,
-            before_move: bool = False,
             is_check: bool = False,
             is_mate: bool = False,
     ):
@@ -28,13 +26,17 @@ class Move:
         self.start = move[0]
         self.end = move[1]
         self.piece = current_board[move[1]]
+        if isinstance(self.piece, NoPiece):
+            raise ValueError
         self.is_drop = is_drop
         self.is_capture = is_capture
         self.is_promote = is_promote
-        self.dropped_piece = dropped_piece
-        self.before_move = before_move
+        if self.is_promote and not self.piece.prom:
+            raise ValueError
         self.is_check = is_check
         self.is_mate = is_mate
+        if not self.is_check and self.is_mate:
+            raise ValueError
         self.tuple = move
         self.string = self.to_string(current_board)
 
@@ -47,16 +49,11 @@ class Move:
             piece_notation = str(self.piece)[0]
         dash = 'x' if self.is_capture else '-'
         if self.is_drop:
-            if self.dropped_piece:
-                dropped_notation = str(self.dropped_piece)[0]
-                notation = f"{dropped_notation}*{self.end}"
-            else:
-                if isinstance(self.piece, NoPiece):
-                    raise ValueError
-                notation = f"{piece_notation}*{self.end}"
+            if isinstance(self.piece, NoPiece):
+                raise ValueError
+            notation = f"{piece_notation}*{self.end}"
         else:
-            other_pieces = self._piece_can_move(
-                current_board, self.piece, self.end)
+            other_pieces = _piece_can_move(current_board, self.piece, self.end)
             notation = piece_notation
             if other_pieces:
                 if all(x.x == self.start.x for x in other_pieces):
@@ -72,24 +69,24 @@ class Move:
             notation += '#' if self.is_mate else '+'
         return notation
 
-    @staticmethod
-    def _piece_can_move(
-            current_board: Board,
-            piece: Piece,
-            to: AbsoluteCoord
-    ) -> List[AbsoluteCoord]:
-        if piece in current_board.pieces.values():
-            pieces = (
-                x for x, y in current_board.pieces.values() if y == piece
+
+def _piece_can_move(
+        current_board: Board,
+        piece: Piece,
+        to: AbsoluteCoord
+) -> List[AbsoluteCoord]:
+    if piece in current_board.pieces.values():
+        pieces = (
+            x for x, y in current_board.pieces.values() if y == piece
+        )
+        valid_spaces = []
+        for location in pieces:
+            cannot_move = move_check_2(
+                current_board,
+                (location, to),
+                ignore_location=to
             )
-            valid_spaces = []
-            for location in pieces:
-                cannot_move = move_check_2(
-                    current_board,
-                    (location, to),
-                    ignore_location=to
-                )
-                if not cannot_move:
-                    valid_spaces.append(location)
-            return valid_spaces
-        return []
+            if not cannot_move:
+                valid_spaces.append(location)
+        return valid_spaces
+    return []
