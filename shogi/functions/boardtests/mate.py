1d314a177925f7f7b1a4247a136201c49e62c036
Patrick Norton <patrick.147.norton@gmail.com>
2019-05-13 07:51:55 -0400

    Changed calculation of has_space in mate_check

--- shogi/functions/boardtests/mate.py
+++ shogi/functions/boardtests/mate.py
@@ -34,6 +34,7 @@ def mate_check(
     # This function tests if any is possible, in that order, as that
     # is the order of increasing difficulty,
 
+    # Generator guards
     if isinstance(ignore_locations, Generator):
         ignore_locations = set(ignore_locations)
     if isinstance(act_full, Generator):
@@ -76,10 +77,12 @@ def mate_check(
             continue
         # If the king can move to the new location, congrats! It's not
         # in check
-        if is_movable(current_board, (king_location, new_location),
-                      ignore_locations=ignore_locations,
-                      act_full=act_full,
-                      piece_pretend=piece_pretend):
+        if is_movable(
+                current_board, (king_location, new_location),
+                ignore_locations=ignore_locations,
+                act_full=act_full,
+                piece_pretend=piece_pretend
+        ):
             return False
     # If there's more than one piece attacking, you can't block
     # or capture, so resistance is futile
@@ -96,7 +99,7 @@ def mate_check(
     not_a_knight = not current_board[check_location].is_rank('n')
     # If the piece is right next to the king, you can't put a piece
     # between it and the king to block it
-    has_space = not all(x in {-1, 0, 1} for x in relative_position)
+    has_space = abs(relative_position.x) > 1 or abs(relative_position.y) > 1
     # If all of these drop-tests are true, it can be blocked
     if has_pieces and not_a_knight and has_space:
         return False
@@ -104,25 +107,26 @@ def mate_check(
     for loc in current_board.enemy_spaces:
         # See if any piece can be moved to the attacking location,
         # to capture the piece
-        if is_movable(current_board, (loc, check_location),
-                      ignore_locations=ignore_locations,
-                      act_full=act_full,
-                      piece_pretend=piece_pretend):
+        if is_movable(
+                current_board, (loc, check_location),
+                ignore_locations=ignore_locations,
+                act_full=act_full,
+                piece_pretend=piece_pretend
+        ):
             return False
     move = king_location - check_location
     move_direction = classes.Direction(move)
     # Test whether or not the piece can be blocked
     # If those tests we saw earlier are false, you can't block the
     # check, and thus, checkmate is true
-    if not (not_a_knight and has_space):
+    if not not_a_knight or not has_space:
         return True
     # Test if any of the player's pieces can move to any of the
     # spaces between the attacking piece and the king
     for pos, z in product(current_board.enemy_spaces, range(max(abs(move)))):
         # The location the piece is moving to:
-        # The location the piece is moving from, plus
-        # z times the move direction, e.g. z spaces away from
-        # the check location in the direction of the move
+        #   The location the piece is moving from, plus z spaces away
+        #   from the location on the direction of the move
         new_location = check_location + move_direction.scale(z)
         # If you can move there and block, no checkmate
         if is_movable(current_board, (pos, new_location),
